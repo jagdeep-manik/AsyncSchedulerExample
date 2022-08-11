e888b13f36f394af57c683dd75d10cbc39a4b1be
Jagdeep Manik <jagdeep.manik@berkeley.edu>
2022-08-11 15:00:42 -0700

    Initial Commit

---
+++ AsyncScheduler/AsyncSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncSchedulerApp.swift
+//  AsyncScheduler
+//
+//  Created by Jagdeep Manik on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncScheduler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncScheduler
+//
+//  Created by Jagdeep Manik on 8/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
